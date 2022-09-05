4e5c1a919872bf451415a3af2def3288c2877e65
Manjusha003 <manjusharaut003@gmail.com>
2022-09-05 22:18:24 +0530

    blog controller

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -3,20 +3,23 @@ const blogsModel = require("../model/blogsModel");
 
 
 const createBlog= async function(req,res){
+    try{
     let blog=req.body;
     let authorId=req.body.authorId;
 
-    if(authorId){
+    if(!authorId)
+        return res.status(400).send({status:false,msg:""})
+
         let authId=await authorModel.find({_id:authorId});
         if(authId){
             let blogCreated=await blogsModel.create(blog);
-            res.send({ status:true,data: blogCreated });
-        }else{
-            res.send({status:false,msg:""})
+            return res.status(201).send({ status:true,data: blogCreated });
         }
-       
     }
-    res.send({status:false,msg:""})
+    catch(err){
+        return res.status(400).send({status:false,msg:err.message})
+    }
+    
 
 }
 
--- src/model/blogsModel.js
+++ src/model/blogsModel.js
@@ -1,27 +1,28 @@
 const mongoose = require('mongoose')
-const Object_id = mongoose.schema.types.ObjectId;
 
-const blogsSchema = new mongoose.schema({
+const Object_id = mongoose.Schema.Types.ObjectId;
+
+const blogsSchema = new mongoose.Schema({
 
 
   title: { type: String, required: true },
   body: { type: String, required: true },
   authorId: {
     type: Object_id,
-    ref: Author
+    ref: "Author"
   },
   tag: [String],
 
   category: { type: String, required: true },
 
-  examples: [String],
+ 
   subcategory: [String],
   deletedAt: {
     type: Date,
     default: null
   },
   isDeleted: { type: Boolean, default: false },
-  //   publishedAt: {type:string}, 
+  publishedAt: { type: Date, default: null },
   isPublished: { type: Boolean, default: false }
 },
 
