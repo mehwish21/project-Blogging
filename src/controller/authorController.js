25cc021b35ecd62de4d787bd2493a1e0f5c6aea6
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2022-09-05 17:50:59 +0530

    author controller

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -1,16 +1,8 @@
 const authorModel= require('../model/authorModel')
 
-//{ fname: { mandatory}, lname: {mandatory}, title: {mandatory, enum[Mr, Mrs, Miss]}, email: {mandatory, valid email, unique}, password: {mandatory} }
 const createAuthor=async function (req, res){
     try {
     const data= req.body;
-
-    // if(!fname) return res.status(204).send({msg: "fname is requried"})
-    // if(!lname) return res.status(204).send({msg: "lname is requried"})
-    // if(!title) return res.status(204).send({msg: "title is requried"})
-    // if(!email) return res.status(204).send({msg: "email is requried"})
-    // if(!password) return res.status(204).send({msg: "password is requried"})
-
     const createAuthor= await authorModel.create(data);
     res.status(201).send({msg : createAuthor})
     }catch(err){
