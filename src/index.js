74e30acff8f80a71d566dba9269f955a5eabfbf4
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2022-09-05 16:03:05 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express =require('express')
+const app= express()
+const bodyParser = require('body-parser');
+//const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://Akhilesh12168:FZeWCA6RSCVaAqQK@cluster0.enuzw59.mongodb.net/group6?retryWrites=true&w=majority"
+, {
+    useNewUrlParser: true
+
+}).then(() =>{console.log("Mongodb is connected")})
+.catch(err => console.log(err))
+
+//app.use('/', route);
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
