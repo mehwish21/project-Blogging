fe363fa320aff5209316930ea53ea5d2c9ecdbe3
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2022-09-05 17:25:21 +0530

    author route

---
+++ src/routes/route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const authorController=require('../controller/authorController');
+
+router.post('/authors', authorController.createAuthor);
+
+module.exports=router;
