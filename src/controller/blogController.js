5131c5f6086c7058006a8bc032cbb38423e08909
Kunallohakare <Kunallohakare92@gmail.com>
2022-09-06 18:27:40 +0530

    done1

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -1,26 +1,91 @@
-const authorModel=require("../model/authorModel");
+const authorModel = require("../model/authorModel");
 const blogsModel = require("../model/blogsModel");
+const mongoose = require('mongoose')
 
 
-const createBlog= async function(req,res){
-    try{
-    let blog=req.body;
-    let authorId=req.body.authorId;
+const createBlog = async function (req, res) {
+    try {
+        let blog = req.body;
 
-    if(!authorId)
-        return res.status(400).send({status:false,msg:""})
+        let authorId = req.body.authorId;
 
-        let authId=await authorModel.find({_id:authorId});
-        if(authId){
-            let blogCreated=await blogsModel.create(blog);
-            return res.status(201).send({ status:true,data: blogCreated });
+
+        if (!authorId)
+            return res.status(400).send({ status: false, msg: "" })
+
+        let authId = await authorModel.find({ _id: authorId });
+        if (authId) {
+            let blogCreated = await blogsModel.create(blog);
+            return res.status(201).send({ status: true, data: blogCreated });
         }
     }
-    catch(err){
-        return res.status(400).send({status:false,msg:err.message})
+    catch (err) {
+        return res.status(400).send({ status: false, msg: err.message })
     }
-    
+
+
 
 }
 
-module.exports.createBlog=createBlog;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const deleteBlogsById = async function (req, res) {
+
+    let Id = req.params.blogId
+
+
+    let blogId = mongoose.Types.ObjectId.isValid(Id)
+    if (blogId == false) return res.send("blogId is incorrect")
+
+    let blog1 = await blogsModel.findById(Id)
+    if (blog1.isDeleted == true) return res.status(404).send()
+
+    let Blog = await blogsModel.findByIdAndUpdate(Id, { $set: { isDeleted: true, deletedAt: new Date() } });
+    if (!Blog) {
+        return res.status(404).send();
+
+    }
+    return res.status(200).send()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports.createBlog = createBlog;
+module.exports.deleteBlogsById = deleteBlogsById
--- src/routes/route.js
+++ src/routes/route.js
@@ -5,5 +5,5 @@ const blogController=require("../controller/blogController")
 
 router.post('/authors', authorController.createAuthor);
 router.post("/blogs",blogController.createBlog)
-
+router.delete("/blogs/:blogId",blogController.deleteBlogsById)
 module.exports=router;
