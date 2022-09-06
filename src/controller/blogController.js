b2d5a5427ce96267fed0c28f0ad0d8598056a311
Manjusha003 <manjusharaut003@gmail.com>
2022-09-06 19:02:53 +0530

    put api added

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -1,14 +1,11 @@
 const authorModel = require("../model/authorModel");
 const blogsModel = require("../model/blogsModel");
-const mongoose = require('mongoose')
 
 
 const createBlog = async function (req, res) {
     try {
         let blog = req.body;
-
         let authorId = req.body.authorId;
-
 
         if (!authorId)
             return res.status(400).send({ status: false, msg: "" })
@@ -24,44 +21,33 @@ const createBlog = async function (req, res) {
     }
 
 
-
 }
 
 
+const updateBlogs = async function (req, res) {
+    try {
+        const blogId = req.params.blogId;
+        const blogData = req.body;
+       
+        if (Object.keys(blogData).length == 0)
+            return res.status(404).send({ status: false, msg: "Body is required" });
 
 
+        let blog = await blogsModel.findOneAndUpdate({ _id: blogId, isDeleted: false },
+            {
+                $set: { isPublished: true, body: blogData.body, title: blogData.title, publishedAt: new Date() },
+                $push: { tags: blogData.tags, subcategory: blogData.subcategory }
+            },
+            { new: true });
 
+        if (!blog)
+            return res.send({ status: false, msg: "blogId is not present" })
+        return res.status(200).send({ status: true, data: blog });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-const deleteBlogsById = async function (req, res) {
-
-    let Id = req.params.blogId
-
-
-    let blogId = mongoose.Types.ObjectId.isValid(Id)
-    if (blogId == false) return res.send("blogId is incorrect")
-
-    let blog1 = await blogsModel.findById(Id)
-    if (blog1.isDeleted == true) return res.status(404).send()
-
-    let Blog = await blogsModel.findByIdAndUpdate(Id, { $set: { isDeleted: true, deletedAt: new Date() } });
-    if (!Blog) {
-        return res.status(404).send();
-
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({ status: false, Error: error.message })
     }
-    return res.status(200).send()
 
 }
 
@@ -69,23 +55,4 @@ const deleteBlogsById = async function (req, res) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports.createBlog = createBlog;
-module.exports.deleteBlogsById = deleteBlogsById
+module.exports = { createBlog, updateBlogs };
